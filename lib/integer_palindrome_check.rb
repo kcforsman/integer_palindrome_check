adc726604da2fa12ce8716980b17376f6f3fe77c
Kaitlin Forsman <kcforsman@outlook.com>
2018-03-29 16:02:53 -0700

    first solution to integer palindrome check

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,28 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+require "pry"
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+  reversed_number = reverse_number(number)
+  if number == reversed_number
+    true
+  else
+    false
+  end
+end
+
+def reverse_number(number)
+  digits = []
+  i = 0
+  while number > 10**i
+    digit = (number/10**(i)) % 10
+    digits << digit
+    i += 1
+  end
+  reversed_number = 0
+  length = digits.length
+  length.times do |x|
+    length -= 1
+    reversed_number += digits[x] * 10**(length)
+  end
+  reversed_number
 end
